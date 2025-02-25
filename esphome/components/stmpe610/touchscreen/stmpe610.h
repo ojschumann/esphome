38e9503475cb98e3b201d9228fc4182242d8e346
Olaf Schumann <o.j.schumann@gmail.com>
2025-02-25 10:43:12 +0100

    first test for smtpe610 controller

---
+++ esphome/components/stmpe610/touchscreen/stmpe610.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include "esphome/components/spi/spi.h"
+#include "esphome/components/touchscreen/touchscreen.h"
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace stmpe610 {
+
+using namespace touchscreen;
+
+class STMPE610Component : public Touchscreen,
+                          public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW,
+                                                spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_2MHZ> {
+ public:
+  /// Set the threshold for the touch detection.
+  void set_threshold(int16_t threshold) { this->threshold_ = threshold; }
+  /// Set the pin used to detect the touch.
+  void set_irq_pin(InternalGPIOPin *pin) { this->irq_pin_ = pin; }
+
+  void setup() override;
+  void dump_config() override;
+  // float get_setup_priority() const override;
+
+ protected:
+
+  int16_t read_adc_(uint8_t ctrl);
+
+  uint16_t get_version_();
+
+  void update_touches() override;
+
+  int16_t threshold_;
+
+  InternalGPIOPin *irq_pin_{nullptr};
+};
+
+}  // namespace stmpe610
+}  // namespace esphome
